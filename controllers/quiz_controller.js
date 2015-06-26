7201e5dda87f2e5ad0275d802392553c1f4fadf5
José Ramón Gago Gómez <jose.ramon.gago.gomez.1991@gmail.com>
2015-06-26 18:58:32 +0200

    Búsquedas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,16 +1,35 @@
 var models = require('../models/models.js');
 
-exports.question = function (req, res) {
-	models.Quiz.findAll().success(function (quiz) {
-		res.render('quizes/question', {pregunta: quiz[0].pregunta});
-	})
+exports.load = function(req, res, next, quizId) {
+	models.Quiz.find(quizId).then(function (quiz) {
+		if (quiz) {
+			req.quiz = quiz;
+			next();
+		} else {
+			next(new Error('No existe quizId=' + quizId));
+		}
+	}).catch(function(error) { next(error); });
+}
+
+exports.index = function(req, res) {
+	var search ="%";
+	if (req.query.search && req.query.search.trim().length > 0) {
+		search = req.query.search.replace(/\s/g,"%");
+		search = ('%' + search + '%').toUpperCase();
+	}
+	models.Quiz.findAll({where:["upper(pregunta) like ?", search], order:"pregunta"}).then(function (quizes) {
+		res.render('quizes/index', { quizes: quizes, search: req.query.search});
+	}).catch(function(error) { next(error); });
+};
+
+exports.show = function (req, res) {
+	res.render('quizes/show', {quiz: req.quiz});
 };
 
 exports.answer = function (req, res) {
-	models.Quiz.findAll().success(function (quiz) {
-		if (req.query.respuesta === quiz[0].respuesta)
-			res.render('quizes/answer', {respuesta: 'Correcto'});
-		else
-			res.render('quizes/answer', {respuesta: 'Incorrecto'});
-	})
+	var resultado = 'Incorrecto';
+	if (req.query.respuesta.toUpperCase() === req.quiz.respuesta.toUpperCase()) {
+		resultado = 'Correcto'
+	} 
+	res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado});
 };
